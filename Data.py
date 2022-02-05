f7164ac0ff42f33927cd060ba84222e7ddd56ad9
DanielCooper0 <s2026670@ed.ac.uk>
2022-02-05 16:03:48 +0000

    data isnt working

--- Data.py
+++ Data.py
@@ -1,13 +1,21 @@
+from random import random
+import re
+
+
 class Data():
 
     players = []
+    completePlayers = []
     round = 0
 
-
-
-    def __init__(self) -> None:
-        self.round = 0
-
-    def addPlayer(player):
+    def addPlayer(self,player):
         self.players.append(player)
-
+        self.completePlayers.append(player)
+    def getPlayers(self):
+        return self.players
+    def nextRound(self):
+        self.round += 1
+    def getRound(self):
+        return round
+    def getRandPlayer(self):
+        self.players.pop(random.choice(self.players))
--- game.py
+++ game.py
@@ -1,15 +1,36 @@
-import player
 
-activePlayers = []
-pastPlayers = activePlayers
+import player
+import Data
+
+
+data = Data()
 
 def startGame():
-    for player in activePlayers:
+    print("the active players are:")
+
+    for player in data.getPlayers():
         if player is not None:
             print(f"{player.getName()}\n", 20)
 
-        
+
+
+    response = input("Do you want to add another player?").tolower()
+
+    while response == "y":
+        newPlayer = player
+        data.addPlayer(newPlayer)
+        response = input("Do you want to add another player?").tolower()
+    playRound()
+
+
+def playRound():
+    data.getRandPlayer()
+
+    
 
 
 
 
+
+
+startGame()
